23e0eb1f676b849932f538e3554ca05fd525d17a
hoetaek <hoetaek@student.snue.ac.kr>
2022-08-21 20:11:50 +0900

    feat: implement search

--- board/views.py
+++ board/views.py
@@ -1,18 +1,21 @@
 from rest_framework.viewsets import ModelViewSet
 from .serializer import CompanySerializer, PostCreateUpdateSerializer, PostListSerializer, PostDetailSerializer
 from .models import Post, Company
+from rest_framework.filters import SearchFilter
+from rest_framework.decorators import action
 
 # Create your views here.
 class PostViewset(ModelViewSet):
     queryset = Post.objects.all()
+    filter_backends = [SearchFilter]
+    search_fields = ['technology', 'company__name', 'position']
 
     def get_serializer_class(self):
         if self.action == 'list':
             return PostListSerializer
         if self.action == 'retrieve':
             return PostDetailSerializer
-        return PostCreateUpdateSerializer  # I dont' know what you want for create/destroy/update.
-
+        return PostCreateUpdateSerializer
 
 class CompanyViewset(ModelViewSet):
     serializer_class = CompanySerializer
